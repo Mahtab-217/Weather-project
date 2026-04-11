d61c1e4830350f17939e71d7fac4606d53777c2b
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-04-11 22:04:20 +0430

    updating codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
+import { Moon, Sun } from "lucide-react";
 import { useEffect, useState } from "react"
 
 export default function App(){
  const [value, setValue]= useState();
   useEffect(()=>{
      async function getWeatherData() {
-     const malomat= await fetch("https://api.openweathermap.org/data/2.5/weather?q=Ghazni&appid=684418efa9274f3ad6491868b0271123")
-      const responce=await malomat.json(); 
+     const malomat= await fetch("https://api.openweathermap.org/data/2.5/weather?q=Ghazni&appid=684418efa9274f3ad6491868b0271123");
+      const responce = await malomat.json(); 
       setValue(responce);
+       
     }
     getWeatherData()
   },[]);
   return (
     <>
     <h1>Here is the weather status of Ghazni for today</h1>
-    {value.weather[0].main}
+   {value.weather[0].main==="Clear" && <Sun/>}
     </>
   )
 }
