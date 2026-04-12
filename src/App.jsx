55d8469d3aaacb27fa821e2164e5e566c17f1f16
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-04-12 21:44:33 +0430

    new style

--- src/App.jsx
+++ src/App.jsx
@@ -23,12 +23,12 @@ export default function App(){
     <div className="w-full h-screen flex justify-center items-center">
       <div className="w-1/2 h-fit p-5 border rounded-md">
     <h1>Here is the weather status of Ghazni for today</h1>
-   {value.weather[0].main==="Clear" ? (<Sun/>):
-   value.weather[0].main==="Clouds" ?(<Cloud/>):
-   value.weather[0].main==="Thunderstorm"?(<CloudSun/>):
-   value.weather[0].main==="Drizzle"?(<CloudDrizzle/>):
-   value.weather[0].main==="Rain"?(<CloudRain/>):
-   value.weather[0].main==="Snow"?(<Snowflake/>):("")
+   {value.weather[0].main==="Clear" ? (<Sun size={42} className="text-yellow-600"/>):
+   value.weather[0].main==="Clouds" ?(<Cloud size={42} className=" text-gray-300" />):
+   value.weather[0].main==="Thunderstorm"?(<CloudSun size={42} className="text-yellow-600"/>):
+   value.weather[0].main==="Drizzle"?(<CloudDrizzle size={42} className="text-yellow-600"/>):
+   value.weather[0].main==="Rain"?(<CloudRain size={42} className="text-yellow-600"/>):
+   value.weather[0].main==="Snow"?(<Snowflake size={42} className="text-yellow-600"/>):("")
    }
    </div>
     </div>
