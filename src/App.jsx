b6af15a4bf039c4c8a10932513e6c140e0931b0d
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-04-14 22:01:00 +0430

    senset and sunrise

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,35 @@ import { useEffect, useState } from "react"
 
 export default function App(){
  const [value, setValue]= useState();
+ const [hour,setHour]= useState("");
+ const [Minute,setMinute]= useState("");
+ const [Seconds,setSeconds]= useState("");
+
+ const [sunsetHour, setSunsetHour]=useState("");
+ const [sunsetMinutes, setSunsetMinute]=useState("");
+ const [sunsetSeconds, setSunsetSeconds]=useState("");
   useEffect(()=>{ 
      async function getWeatherData() {
-     const malomat= await fetch("https://api.openweathermap.org/data/2.5/weather?q=Kabul&units=metric&appid=684418efa9274f3ad6491868b0271123");
+     const malomat= await fetch("https://api.openweathermap.org/data/2.5/weather?q=Ghazni&units=metric&appid=684418efa9274f3ad6491868b0271123");
       const responce = await malomat.json(); 
       setValue(responce);
-       
+     const sunRiseDate= new Date(responce.sys.sunrise * 1000);
+     const sunRiseHour= sunRiseDate.getHours();
+       const sunRiseMinute = sunRiseDate.getMinutes();
+       const sunRiseSeconds = sunRiseDate.getSeconds();
+      setHour(sunRiseHour);
+       setMinute(sunRiseMinute);
+       setSeconds(sunRiseSeconds);
+
+      //  sunsetDate
+      const sunSetDate= new Date(responce.sys.sunset * 1000);
+      const sunSetHour = sunSetDate.getHours();
+      const sunSetMinute = sunSetDate.getMinutes();
+      const sunSetSeconds = sunSetDate.getSeconds();
+      setSunsetHour(sunSetHour)
+      setSunsetMinute(sunSetMinute)
+      setSunsetSeconds(sunSetSeconds)
+
     }
     getWeatherData()
   },[]);
@@ -22,7 +45,7 @@ export default function App(){
   return (
     <div className="w-full h-screen flex justify-center items-center">
       <div className="w-1/2 h-fit p-5 border rounded-md">
-    <h1>Here is the weather status of Ghazni for today</h1>
+    <h1>{value.name} City</h1>
    {value.weather[0].main==="Clear" ? (<Sun size={42} className="text-yellow-600"/>):
    value.weather[0].main==="Clouds" ?(<Cloud size={42} className=" text-gray-300" />):
    value.weather[0].main==="Thunderstorm"?(<CloudSun size={42} className="text-yellow-600"/>):
@@ -45,10 +68,12 @@ export default function App(){
 <div className="flex w-full justify-between items-center">
   <p>
     <span>Sunrise</span>
+    <span>{hour}:{Minute}:{Seconds}</span>
   </p>
 
-  <p>
-    <span>Sunset</span>
+  <p className="flex w-full justify-between items-center">
+    <span>Sunset:</span>
+    <span>{sunsetHour}:{sunsetMinutes}:{sunsetSeconds}</span>
   </p>
 </div>
 
