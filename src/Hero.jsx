2f712557e6029f35b4fd67bdbdb15da94016bc2d
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-04-16 21:39:57 +0430

    new style for hero part

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,7 +1,8 @@
 export default function(){
     return (
-        <div>
-            <h1>This massage comes from the Hero part</h1>
+        <div className="w-full h-screen flex flex-col justify-around items-center ">
+            <h1 className="text-3xl fonr-bold w-fit text-red-600 text-shadow-[3px_2px-3px_black]">This massage comes from the Hero part</h1>
+            <p className="text-center text-2xl font-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ipsa doloremque laborum et at quas expedita temporibus nulla earum aliquam, qui quaerat, obcaecati vero illum aperiam quisquam error consectetur laudantium!</p>
         </div>
     )
 }
